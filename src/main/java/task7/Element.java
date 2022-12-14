613d8dd25175f1aceef6767ac8b796e7ec7defd8
osshersh <olegshershnov@gmail.com>
2022-12-14 14:06:20 +0100

    Zrobiono task7

---
+++ src/main/java/task7/Element.java
@@ -0,0 +1,11 @@
+package task7;
+
+public class Element {
+
+    public static double getElement(double value) {
+        if (value < 0) {
+            throw new IllegalArgumentException();
+        } else
+            return Math.sqrt(value);
+    }
+}
