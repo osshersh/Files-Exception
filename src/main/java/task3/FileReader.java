09c2c9959151a932afa5b9ac05618553c31cfbbc
osshersh <olegshershnov@gmail.com>
2022-12-15 22:32:30 +0100

    Przerobiono task3

--- src/main/java/task3/FileReader.java
+++ src/main/java/task3/FileReader.java
@@ -1,6 +1,5 @@
 package task3;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,12 +8,8 @@ public class FileReader {
 
     public static void calculateSignInFile(String filePath) {
         try {
-            String sign = "";
-            String[] fileContent = Files.readString(Paths.get(filePath)).split("\n");
-            for (int i = 0; i <= fileContent.length - 1; i++) {
-                sign = sign + fileContent[i];
-            }
-            System.out.println(sign.length());
+            String fileContent = Files.readString(Paths.get(filePath));
+            System.out.println(fileContent.length());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
