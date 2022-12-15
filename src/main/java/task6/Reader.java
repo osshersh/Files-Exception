23d0e04784f51a254c1abfd8e94611f114419d32
osshersh <olegshershnov@gmail.com>
2022-12-15 23:22:20 +0100

    Przerobiono task6

--- src/main/java/task6/Reader.java
+++ src/main/java/task6/Reader.java
@@ -11,25 +11,30 @@ public class Reader {
         File data = new File("C:\\Files-Exception\\src\\main\\resources\\data.txt");
         File output = new File("C:\\Files-Exception\\src\\main\\resources\\output.txt");
 
-        readFile(data.getPath(),output.getPath());
+        String fileContent = convertFileContentToUpperCase(data.getPath());
+        writeFile(output.getPath(), fileContent);
     }
 
-    public static void readFile(String filePathRead, String filePathWrite) {
-        String fileContentsToUpper = "";
+    public static String readFile(String filePath) {
+        if (filePath == null) {
+            throw new NullPointerException("Path can't null");
+        }
         try {
-            fileContentsToUpper = Files.readString(Paths.get(filePathRead)).toUpperCase();
-            writeFile(filePathWrite, fileContentsToUpper);
+            return Files.readString(Paths.get(filePath));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     private static void writeFile(String filePath, String fileContents) {
-
         try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(filePath))) {
-             bufferedWriter.write(fileContents);
+            bufferedWriter.write(fileContents);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String convertFileContentToUpperCase(String filePath) {
+        return readFile(filePath).toUpperCase();
     }
 }
