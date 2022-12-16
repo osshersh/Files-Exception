565331b4d86d497feb9bfe0765e77fb7e5037539
osshersh <olegshershnov@gmail.com>
2022-12-16 16:25:49 +0100

    Korekta task8

--- src/main/java/task8/PersonalNumber.java
+++ src/main/java/task8/PersonalNumber.java
@@ -4,11 +4,30 @@ public class PersonalNumber {
 
     public static String getSocialNumber(String number) {
         String regex = "\\d+";
+
         if (number == null || !number.matches(regex)) {
-            throw new IllegalArgumentException("Incorrect data type");
+            throw new WrongTypeOfDataException("Incorrect data type");
         } else if (number.length() != 10) {
-            throw new ArrayIndexOutOfBoundsException("Wrong length");
+            throw new IllegalLengthException("Wrong length");
         }
         return number;
+    }
+
+    private static class WrongTypeOfDataException extends IllegalArgumentException {
+        public WrongTypeOfDataException() {
+        }
+
+        public WrongTypeOfDataException(String s) {
+            super(s);
+        }
+    }
+
+    private static class IllegalLengthException extends ArrayIndexOutOfBoundsException {
+        public IllegalLengthException() {
+        }
+
+        public IllegalLengthException(String s) {
+            super(s);
+        }
     }
 }
